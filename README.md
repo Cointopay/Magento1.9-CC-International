26648d2860d1fbe3cf51e2f221b69e8a38a5398b
trswebteam <goshila.sadaf@therightsw.com>
2020-07-07 23:52:40 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Official Cointopay Magento 1.9 CC International plugin
+ Download magento zip via the green button above (direct link: https://github.com/Cointopay/Magento1.9-CC-International/archive/master.zip)!
+
+Please follow these Magento install instructions:
+https://github.com/Cointopay/Magento1.9/blob/master/cointopayintlccgateway_magento19_documentationv0.02.pdf
+
+Support update: In Magent1.9-CC-International-master/app/code/local/CointopayIntlCC/Paymentgateway/Model/Observer.php
+>> please update yourdomain.com to e.g. myshop.com >>transactionconfirmurl=https://yourdomain.com/cointopayintlccgateway/order/&transactionfailurl=https://yourdomain.com/cointopayintlccgateway/order/
+
+# Support or Contact
+Please contact support@cointopay.com for feature requests and/or incidents, or create a ticket here: https://cointopay.freshdesk.com/helpdesk
+
+# About Cointopay.com
+We are an international crypto currency payment processor, meaning that we accept payments from your customers and make the funds available to you (incl. in form of fiat currency like euro). The direct integration with Magento provides you with a seamless payment experience while underlying dealing with diverse and complex blockchain technologies like Bitcoin, Ethereum, Neo, Dash, Ripple and many more. P.S. If you want your own crypto currency to become available in this plugin, we can provide that for you as well, Cointopay has been a technological payment incubator since 2014!
---
+++ app/code/local/CointopayIntlCC/Paymentgateway/controllers/PaymentController.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright © 2018 Cointopay. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+class CointopayIntlCC_Paymentgateway_PaymentController extends Mage_Core_Controller_Front_Action {
+	// The redirect action is triggered when someone places an order
+	// public function redirectAction() {
+	public function successAction() {
+		$order = new Mage_Sales_Model_Order();
+		$orderId = Mage::getSingleton('checkout/session')->getLastRealOrderId();
+		$order->loadByIncrementId($orderId);
+		$coinToPayResponse = Mage::getSingleton('core/session')->getCointopayresponse();
+		$coinsResponse = Mage::getSingleton('core/session')->getCoinresponse();
+		$orderresponse = json_decode($coinsResponse);
+		if($order->getIncrementId()) {
+		$amount = number_format($orderresponse->OriginalAmount, 2);
+		$url = "https://surplus17.com:9443/ctpv2/?call=stripe&transactionID={$orderresponse->TransactionID}&amount={$amount}&currency=EUR&customerReferenceNr={$order->getIncrementId()}&confirmCode={$orderresponse->Security}";
+		Mage::app()->getResponse()->setRedirect($url)->sendResponse();
+		}
+	}
+	
+	// The response action is triggered when your gateway sends back a response after processing the customer's payment
+	public function responseAction() {
+		if($this->getRequest()->isPost()) {
+			
+			/*
+			/* Your gateway's code to make sure the reponse you
+			/* just got is from the gatway and not from some weirdo.
+			/* This generally has some checksum or other checks,
+			/* and is provided by the gateway.
+			/* For now, we assume that the gateway's response is valid
+			*/
+			
+			$validated = true;
+			$orderId = '123'; // Generally sent by gateway
+			
+			if($validated) {
+				// Payment was successful, so update the order's state, send order email and move to the success page
+				$order = Mage::getModel('sales/order');
+				$order->loadByIncrementId($orderId);
+				$order->setState(Mage_Sales_Model_Order::STATE_PROCESSING, true, 'Gateway has authorized the payment.');
+				
+				$order->sendNewOrderEmail();
+				$order->setEmailSent(true);
+				
+				$order->save();
+			
+				Mage::getSingleton('checkout/session')->unsQuoteId();
+				
+				Mage_Core_Controller_Varien_Action::_redirect('checkout/onepage/success', array('_secure'=>true));
+			}
+			else {
+				// There is a problem in the response we got
+				$this->cancelAction();
+				Mage_Core_Controller_Varien_Action::_redirect('checkout/onepage/failure', array('_secure'=>true));
+			}
+		}
+		else
+			Mage_Core_Controller_Varien_Action::_redirect('');
+	}
+	
+	// The cancel action is triggered when an order is to be cancelled
+	public function cancelAction() {
+        if (Mage::getSingleton('checkout/session')->getLastRealOrderId()) {
+            $order = Mage::getModel('sales/order')->loadByIncrementId(Mage::getSingleton('checkout/session')->getLastRealOrderId());
+            if($order->getId()) {
+				// Flag the order as 'cancelled' and save it
+				$order->cancel()->setState(Mage_Sales_Model_Order::STATE_CANCELED, true, 'Gateway has declined the payment.')->save();
+			}
+        }
+	}
+}
